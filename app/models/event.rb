53c1c6bd509cebac9f159172d4ec8845cf2a288d
Aaron Berns <anberns@gmail.com>
2019-03-04 09:29:31 -0500

    add relationships, validation to Event, Team models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,15 @@
 class Event < ApplicationRecord
+  has_many :user_events
+  has_many :users, through: :user_events, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true 
+  validates :distance, presence: true
+  validates :stroke, presence: true
+
+  scope :filter_by_distance, ->(distance) {where("distance <= ?", distance)}
+  scope :filter_by_stroke, ->(stroke) {where("stroke = ?", stroke.downcase)}
+
+  def self.sort
+    sorted_events = Event.all.sort_by { |event| event.distance }
+  end
 end
