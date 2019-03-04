405953a8ed3dcec9d7661f2465d0c2de6869505e
Aaron Berns <anberns@gmail.com>
2019-03-04 18:17:06 -0500

    add href to team cards

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -15,9 +15,29 @@ function attachTeamsListeners() {
 
 function loadTeamsLink() {
   let teamsLink = document.getElementById('teams_link');
-
   teamsLink.addEventListener("click", (e) => { showTeams(e) })
+}
 
+function loadTeamCardLinks() {
+  let images = document.getElementsByClassName("team-image");
+  for (let i of images) {
+    i.addEventListener("click", (e) => { showTeam(e) });
+  }
+}
+
+
+function showTeam(e) {
+e.preventDefault();
+  $.ajax({
+    type: 'GET',
+    url: e.target.id,
+    success: (response) => {
+      let newDiv = createNewDiv("team_page")
+      let template = Handlebars.compile(document.getElementById('team-show-template').innerHTML)
+      let team = template(response.data)
+      newDiv.innerHTML += team
+      }
+    });
 }
 
 function showTeams(e) {
@@ -32,5 +52,5 @@ function showTeams(e) {
       newDiv.innerHTML += teams
       }
     });
-
+    loadTeamCardLinks();
 }
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,4 +4,9 @@ class TeamsController < ApplicationController
     teams = Team.all 
     render json: teams
   end
+
+  def show
+    team = Team.find(params[:id])
+    render json: team 
+  end
 end
