2ba7e2898990b3819709d7b68cb6a18a610cfb30
Aaron Berns <anberns@gmail.com>
2019-03-04 14:55:44 -0500

    add event edit template

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -4,18 +4,41 @@ document.addEventListener('DOMContentLoaded', attachEventsListeners);
 
 function attachEventsListeners() {
   loadEventsLink();
+  loadEventEditLinks();
 }
 
 function loadEventsLink() {
   let eventsLink = document.getElementById('events_link');
-
   eventsLink.addEventListener("click", (e) => { showEvents(e) })
+}
 
+function loadEventEditLinks() {
+  let editButtons = document.getElementsByClassName("edit_button");
+  for (let button of editButtons) {
+    button.addEventListener("click", (e) => { editEvent(e) })
+  }
+}
+
+function editEvent(e) {
+  e.preventDefault();
+  $.ajax({
+    type: 'GET',
+    url: e.target.id,
+    success: (response) => {
+      let contentDiv = document.getElementById('main_content')
+      contentDiv.innerHTML = "";
+      let newDiv = document.createElement('div');
+      newDiv.id = "event_edit_form"
+      contentDiv.appendChild(newDiv)
+      let template = Handlebars.compile(document.getElementById('event-edit-template').innerHTML); 
+      let event = template(response.data)
+      newDiv.innerHTML += event
+    }
+  })
 }
 
 function showEvents(e) {
   e.preventDefault();
-  e.stopPropagation();
   $.ajax({
     type: 'GET',
     url: `/events`,
