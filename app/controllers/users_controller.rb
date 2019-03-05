88b47974d8344de2884d479986af3fbceb517eef
Aaron Berns <anberns@gmail.com>
2019-03-05 10:05:35 -0500

    add index action to users controller that handles nested route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     render json: user
   end
+
+  def index
+    if params[:team_id]
+      users = Team.find(params[:team_id]).users
+    else
+      users = Users.all 
+    end
+    render json: users
+  end
+
 end
