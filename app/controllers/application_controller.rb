b658a0d02f4e731d606157e0fc324c32b6102a9f
Aaron Berns <anberns@gmail.com>
2019-03-05 18:07:35 -0500

    prepare to branch off into authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def current_swimmer
+    current_swimmer ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  helper_method :current_swimmer
 end
