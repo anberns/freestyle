c607602c95f76e74ac45e2cfe3809b53828c9274
Aaron Berns <anberns@gmail.com>
2019-03-05 15:29:35 -0500

    add events to profile template

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :name, :team 
+  attributes :id, :email, :name, :team, :events 
   belongs_to :team
   has_many :user_events 
   has_many :events, through: :user_events
