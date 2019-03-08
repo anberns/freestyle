25f0d6cf844eaa4d49a7e807bcfceee1dda77c9c
Aaron Berns <anberns@gmail.com>
2019-03-08 09:26:22 -0500

    remove sessions function from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-  def current_swimmer
-    current_swimmer ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  
-  helper_method :current_swimmer
 end
